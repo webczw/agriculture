5d83aa7a73ba7b176d229fe56bd14c7e2f835e5b
webczw <webczw@163.com>
2018-06-16 20:51:58 +0800

    整合swagger2，合并代码

--- src/main/java/com/hjd/power/agriculture/controller/DefaultController.java
+++ src/main/java/com/hjd/power/agriculture/controller/DefaultController.java
@@ -2,22 +2,40 @@ package com.hjd.power.agriculture.controller;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.hjd.power.agriculture.domain.SimpleMailMessageVO;
+import com.hjd.power.agriculture.service.IMailService;
+
+import io.swagger.annotations.ApiOperation;
 
 @RestController
 @RequestMapping("/main")
 public class DefaultController {
 	private Logger logger = LoggerFactory.getLogger(DefaultController.class);
+	@Autowired
+	private IMailService mailService;
+
+	@ApiOperation(value = "日志测试", notes = "根据入参内容测试日志")
 	@GetMapping("/test/{name}")
 	public String test(@PathVariable("name") String name) throws Exception {
-		logger.trace(name+"日志输出 trace");
-        logger.debug(name+"日志输出 debug.");
-        logger.info(name+"日志输出 info.");
-        logger.warn(name+"日志输出 warn.");
-        logger.error(name+"日志输出 error.");
-		return "My , "+ name;
+		logger.trace(name + "日志输出 trace");
+		logger.debug(name + "日志输出 debug.");
+		logger.info(name + "日志输出 info.");
+		logger.warn(name + "日志输出 warn.");
+		logger.error(name + "日志输出 error.");
+		return "My , " + name;
+	}
+
+	@ApiOperation(value = "发送邮件", notes = "根据入参内容发送邮件")
+	@PostMapping("/send")
+	public void send(@RequestBody SimpleMailMessageVO vo) throws Exception {
+		mailService.sendSimpleMail(vo);
 	}
 }
