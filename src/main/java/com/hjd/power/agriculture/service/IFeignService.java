6e8d54171a8750d9726ee08258888de3179c35cb
webczw <webczw@163.com>
2018-07-18 00:05:30 +0800

    调整发送请求

--- src/main/java/com/hjd/power/agriculture/service/IFeignService.java
+++ src/main/java/com/hjd/power/agriculture/service/IFeignService.java
@@ -7,10 +7,10 @@ import org.springframework.web.bind.annotation.RequestParam;
 
 @FeignClient(url = "${agriculture.control.url}", name = "light")
 public interface IFeignService {
-	@RequestMapping(value = "/controlLightHouse", method = RequestMethod.GET)
+	@RequestMapping(value = "/controlLightHouse", method = RequestMethod.GET, produces = "application/json; charset=UTF-8")
 	public String connect();
 
-	@RequestMapping(value = "/controlLightHouse", method = RequestMethod.GET)
+	@RequestMapping(value = "/controlLightHouse", method = RequestMethod.GET, produces = "application/json; charset=UTF-8")
 	public String updateLightHouse(@RequestParam("site_code") String site_code,
 			@RequestParam("on_off_flag") Integer on_off_flag, @RequestParam("site_code") Integer delay,
 			@RequestParam("boot_date_delay") Integer boot_date_delay);
